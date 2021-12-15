197145e0d82b41a1fd6f3f84ca8b132f6a0e4a1d
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-12-15 01:15:09 -0600

    fixed data transfer to front end so that it can be parsed via json on the front.

--- app/controllers/live_stream_echos_controller.rb
+++ app/controllers/live_stream_echos_controller.rb
@@ -1,9 +1,13 @@
 class LiveStreamEchosController < ApplicationController
   def index
     require "json"
-    lines = File.open("out.txt").read.split("}\n").join("},")+"}"
+    lines = File.open("out.txt").read
     data = JSON.parse([lines].to_s)
-    render json: data
+    p "data"
+    p data
+
+    # render json: data[0].as_json
+    render json: data[0]
   end
   
 end
