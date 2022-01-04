8957007f4d997185479f2843dcd662f4f6b0459d
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2022-01-04 09:13:45 -0600

    code

--- app/controllers/dynasty_nerds_news_controller.rb
+++ app/controllers/dynasty_nerds_news_controller.rb
@@ -1,9 +1,10 @@
 class DynastyNerdsNewsController < ApplicationController
   
-  #Controller for RSS Feed from Dynasty Nerds.  This is for NFL specifically.  Running on backend to avoid CORS issues.
+  #Controller for RSS Feed from Dynasty Nerds.  This is for NFL specifically.  Running on backend to avoid CORS issues. comment
 
   def index
     response = HTTP.get("https://www.dynastynerds.com/feed/")
     render json: response.to_s
   end
+
 end
